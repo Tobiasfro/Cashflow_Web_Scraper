5eed7ea060f9429f204b0564e9f4a867596cbd4f
Tobias Fröberg <tobias@frobergs.org>
2022-07-25 11:57:23 +0200

    Code refactor and clean up

--- cashflow_scraper.py
+++ cashflow_scraper.py
@@ -1,12 +1,13 @@
-"""cashflow.py: Goes through x number of pages in cashflow and finds all Studs22 receipts 
+'''cashflow.py: Goes through x number of pages in cashflow and finds all Studs22 receipts
                 that HAS been certified but NOT yet confirmed to be in the ledger.
                 Prints the URL's to these receipts in a .txt file. Also prints (in the terminal)
-                the number of found unconfirmed receipts and the total cost of them in SEK."""
+                the number of found unconfirmed receipts and the total cost of them in SEK.'''
 
-__author__ = "Tobias Fröberg"
+__author__ = 'Tobias Fröberg'
 
-import requests
+import sys
 import time
+import requests
 from bs4 import BeautifulSoup
 
 # Insert your cookies from your GET request.
@@ -14,53 +15,70 @@ cookies = {
 }
 
 def main():
-    response = requests.get('https://cashflow.datasektionen.se/admin/expenses/?page=1&committee=STUDS%202022', cookies=cookies)
+    '''Asks the user how many pages to scan and finds all the receipts that HAS been certified
+    but NOT yet confirmed to be in the ledger. Prints the URL's to these receipts cashflow.txt.
+    Also prints (in the terminal) the number of found unconfirmed receipts and the total cost
+    of them in SEK.'''
+
+    response = requests.get('https://cashflow.datasektionen.se/admin/expenses/?page=1&'
+        'committee=STUDS%202022', cookies=cookies)
     soup = BeautifulSoup(response.content, 'html5lib')
-    maxPageNum = max(1,len(soup.find('span', attrs = {'class':'step-links'}).find_all('a'))-1)
+    max_page_num = max(1,len(soup.find('span', attrs = {'class':'step-links'}).find_all('a'))-1)
 
     try:
         # If for example 5 is given, page nr 1, 2, 3, 4 and 5 of the receipts will be scanned.
-        pageNum = int(input("Insert the number of pages (1-" + str(maxPageNum) + ") to scan: "))
+        page_num = int(input('Insert the number of pages (1-' + str(max_page_num) + ') to scan: '))
     except ValueError:
-        print("Input has to be a number.")
-        quit()
+        sys.exit('Input has to be a number.')
 
-    if maxPageNum < pageNum or pageNum < 1:
-        print("You submitted an invalid number, submit a number in the range of 1-" + str(maxPageNum) + ".")
-        quit()
+    if max_page_num < page_num or page_num < 1:
+        sys.exit('You submitted an invalid number, submit a number in the range of 1-'
+            + str(max_page_num) + '.')
 
-    totUnconfirmReceipts = 0
-    totalReceiptCost = 0.0
-    f = open("cashflow.txt", "w")
-    while pageNum >= 1:
-        response = requests.get('https://cashflow.datasektionen.se/admin/expenses/?page='+str(pageNum)+'&committee=STUDS%202022', cookies=cookies)
+    tot_unconfirm_receipts = 0
+    total_receipt_cost = 0.0
+    file = open('cashflow.txt', 'wt', encoding='utf8')
+    while page_num >= 1:
+        response = requests.get('https://cashflow.datasektionen.se/admin/expenses/?page='
+            +str(page_num)+'&committee=STUDS%202022', cookies=cookies)
         soup = BeautifulSoup(response.content, 'html5lib')
-        newUnconfirmReceipts = 0
+        new_unconfirm_receipts = 0
 
         for tag in soup.find_all('tr'):
             for link in tag.find_all('a'):
-                if link.has_attr('href') and link.text == "Attesterad men inte i pärmen":
-                    f.write("https://cashflow.datasektionen.se" + link.attrs['href'] + "\n")
-                    newUnconfirmReceipts += 1
+                # Only interested in the receipts that HAS been certified but NOT yet 
+                # confirmed to be in the ledger.
+                if link.has_attr('href') and link.text == 'Attesterad men inte i pärmen':
+                    file.write('https://cashflow.datasektionen.se' + link.attrs['href'] + '\n')
+                    new_unconfirm_receipts += 1
 
-                    # Add the cost of this receipt to the total cost of the previous found unconfirmed receipts.
-                    costString = tag.find('td', attrs = {'class':'right'}).text.split(" ")[0]
-                    costFloat = float(replacer(costString))
-                    totalReceiptCost += costFloat
-        
+                    # Add the cost of this receipt to the total cost of the previous 
+                    # found unconfirmed receipts.
+                    total_receipt_cost += receipt_cost_to_float(tag)
+
         time.sleep(1/4)
-        totUnconfirmReceipts += newUnconfirmReceipts
-        print("Done with page " + str(pageNum) + ". " + str(newUnconfirmReceipts) + " new unconfirmed receipts found.")
-        print("The total is now " + str(totUnconfirmReceipts) + " receipts with the cost of "+ str(round(totalReceiptCost,2)) + " kr.\n")
-        pageNum -= 1
+        tot_unconfirm_receipts += new_unconfirm_receipts
+        print('Done with page ' + str(page_num) + '. ' + str(new_unconfirm_receipts)
+            + ' new unconfirmed receipts found.')
+        print('The total is now ' + str(tot_unconfirm_receipts) + ' receipts with the cost of '
+            + str(round(total_receipt_cost,2)) + ' kr.\n')
+        page_num -= 1
 
-    f.close()
+    file.close()
 
-def replacer(str):
-    # Replaces the unwanted characters in the string so that the float function can convert the string to float.
+def receipt_cost_to_float(tag):
+    '''Finds and converts the cost of the receipt (string) to float.'''
+
+    cost_string = tag.find('td', attrs = {'class':'right'}).text.split(' ')[0]
+    return float(replacer(cost_string))
+
+def replacer(cost_string):
+    '''Replaces the unwanted characters in the string so that the float
+    function can convert the string to float.'''
+
     replacers = {',':'.', '\xa0':''}
-    for ch in replacers:
-        str = str.replace(ch, replacers[ch])
-    return str
+    for key, value in replacers.items():
+        cost_string = cost_string.replace(key, value)
+    return cost_string
 
 main()
